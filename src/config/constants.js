794481a0e28081a72fea4683f4b2e8d3f5011569
CookieMasterT <tytus@baranscy.info>
2026-09-03 15:59:11 +0200

    Change default model to smollm2

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,4 @@
 export const DEFAULT_SYSTEM_PROMPT =
   "You are a helpful AI agent helping users.";
-export const DEFAULT_MODEL = "Llama-3-8B-Instruct-q4f32_1-MLC-1k";
+export const DEFAULT_MODEL = "SmolLM2-1.7B-Instruct-q4f16_1-MLC";
 export const VIEW_NAVS = ["setup", "chat", "insights"];
